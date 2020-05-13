f2c3c2bcac083d6a13ae51d4c2778f876c75259a
Kosteez <60351605+Kosteez@users.noreply.github.com>
2020-05-13 17:20:37 -0400

    Add files via upload

--- FinalProg1HangmanGame.java
+++ FinalProg1HangmanGame.java
@@ -28,5 +28,89 @@ public class FinalProg1HangmanGame {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        while (count < 7 && asterisk.contains("*")) {
+            System.out.println();
+            System.out.println("Guess any letter in the word");
+            System.out.println(asterisk);
+            String guess = sc.next();
+            hang(guess);
+        } sc.close();
+    }
+    
+    // replacing the letters and asterisks
+    public static void hang(String guess) {
+        String newasterisk = "";
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == guess.charAt(0)) {
+                newasterisk += guess.charAt(0);
+            } else if (asterisk.charAt(i) != '*') {
+                newasterisk += word.charAt(i);
+            } else {
+                newasterisk += "*";
+            }
+        }
+        
+        // correct guess output
+        if (asterisk.equals(newasterisk)) {
+            count++;
+            hangmanResult();
+        } else {
+            asterisk = newasterisk;
+        }
+        if (asterisk.equals(word)) {
+            System.out.println();
+            System.out.println("Correct! You win! The word was " + word);
+        
+            if (count <= 2 && count != 0) {
+                System.out.println("You're a champion! ");
+                System.out.println("It only took you " + count + " wrong guesses! ");
+            } else if (count > 2 && count < 5 ) {
+                System.out.println("You're a novice guesser, keep it up! ");
+                System.out.println("It only took you " + count + " wrong guesses! ");
+            } else if (count >= 5) {
+                System.out.println("You're not a very guesser, but practice makes perfect! Don't give up! ");
+                System.out.println("It only took you " + count + " wrong guesses! ");
+            } else if (count == 0) {
+                System.out.println("Flawless! ");
+                System.out.println("You guessed perfectly! ");
+            }
+        }
+    }
+    
+    // false guess output
+    public static void hangmanResult() {
+        switch (count) {
+            case 1:
+                System.out.println("Wrong guess, try again");
+                System.out.println();
+                break;
+            case 2:
+                System.out.println("Wrong guess, try again");
+                System.out.println();
+                break;
+            case 3:
+                System.out.println("Wrong guess, try again");
+                System.out.println();
+                break;
+            case 4:
+                System.out.println("Wrong guess, try again");
+                System.out.println();
+                break;
+            case 5:
+                System.out.println("Wrong guess, try again");
+                System.out.println();
+                break;
+            case 6:
+                System.out.println("Wrong guess, try again");
+                System.out.println();
+                break;
+            case 7:
+                System.out.println("GAME OVER!");
+                System.out.println("GAME OVER! The word was " + word);
+                break;
+            default:
+                break;
+        }
     }
 }
